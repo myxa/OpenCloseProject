6d073fc793e0fc558df1fc97cc9737905d4ea085
myxa <tanai2010@mail.ru>
2023-08-31 20:13:48 +0300

    rename ipynb

---
+++ utils/helpers.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def reorder_matrix(matr, ids):
+    sort_0 = matr[ids]
+    return sort_0.T[ids]
+
+def plot_corr_hist(ts1, ts2, title=''):
+    l=[]
+    for i in range(423):
+        l.append(np.corrcoef(ts1[i], ts2[i])[0, 1])
+    plt.hist(l, bins='auto')
+    plt.title(f'{title}, median={round(np.median(l), 3)}');
